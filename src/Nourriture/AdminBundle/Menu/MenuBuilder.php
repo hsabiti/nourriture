125be6746bb7b12cb73e86ed3bbf673f89fec70b
Henry Sabiti <hsabiti@hotmail.com>
2013-04-26 22:06:35 +0100

    work in progress

--- src/Nourriture/AdminBundle/Menu/MenuBuilder.php
+++ src/Nourriture/AdminBundle/Menu/MenuBuilder.php
@@ -4,22 +4,27 @@ namespace Nourriture\AdminBundle\Menu;
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\DependencyInjection\ContainerAware;
-
 class MenuBuilder extends ContainerAware
 {
     private $factory;
+    private $translator;
 
     /**
      * @param FactoryInterface $factory
      */
-    public function __construct(FactoryInterface $factory)
+    public function __construct(FactoryInterface $factory, $translator)
     {
-        $this->factory = $factory;
+        $this->factory      = $factory;
+	$this->translator   = $translator;
     }
 
     public function createMainMenu(Request $request)
     {
 
+	$t = $this->translator;
+
+#print $t->getLocale();
+#die($t->trans('dashboard.dashboard'));
 	#print_r($request->getLocale());print "\n";die(__FILE__.__LINE__);
         #$menu = $this->factory->createItem('root', array('attributes'=>array('class'=>'nav')));
         $menu = $this->factory->createItem('root');
@@ -30,23 +35,23 @@ class MenuBuilder extends ContainerAware
 
 	#print_r(basename($request->getRequestUri()));die(__FILE__.__LINE__);
 
-        $menu->addChild('dashboard', array('label'=>'Dashboard','route' => 'admin_dashboard_list'));
-		$menu['dashboard']->addChild('list', array('label'=>'List', 'route'=>'admin_dashboard_list', 'attributes'=>array('class'=>'dashboard')));
+        $menu->addChild('dashboard', array('label'=> $t->trans('dashboard.dashboard'),'route' => 'admin_dashboard_list'));
+		$menu['dashboard']->addChild('list', array('label'=>$t->trans('dashboard.list'), 'route'=>'admin_dashboard_list', 'attributes'=>array('class'=>'dashboard')));
 		$menu['dashboard']->setChildrenAttributes(array('class'=>'subnav'));
 
 
 
-        $menu->addChild('products', array('label'=>'Products','route' => 'admin_products_list'));
-		$menu['products']->addChild('list', array('label'=>'List Products', 'route'=>'admin_products_list', 'attributes'=>array('class'=>'placeholder')));
-		$menu['products']->addChild('add', array('label'=>'Add New Product','route'=>'admin_products_add', 'attributes'=>array('class'=>'pissoff')));
+        $menu->addChild('products', array('label'=>$t->trans('dashboard.products'),'route' => 'admin_products_list'));
+		$menu['products']->addChild('list', array('label'=>$t->trans('dashboard.list_products'), 'route'=>'admin_products_list', 'attributes'=>array('class'=>'placeholder')));
+		$menu['products']->addChild('add', array('label'=>$t->trans('dashboard.add_product'),'route'=>'admin_products_add', 'attributes'=>array('class'=>'pissoff')));
 
-	$menu->addChild('users', array('label'=>'Users','route' => 'admin_users_list'));
-		$menu['users']->addChild('users', array('label'=>'List Users', 'route'=>'admin_users_list', 'attributes'=>array('class'=>'users')));
-		$menu['users']->addChild('add', array('label'=>'Add New User', 'route'=>'admin_users_add', 'attributes'=>array('class'=>'placeholder')));
+	$menu->addChild('users', array('label'=>$t->trans('dashboard.users'),'route' => 'admin_users_list'));
+		$menu['users']->addChild('users', array('label'=>$t->trans('dashboard.list_users'), 'route'=>'admin_users_list', 'attributes'=>array('class'=>'users')));
+		$menu['users']->addChild('add', array('label'=>$t->trans('dashboard.add_user'), 'route'=>'admin_users_add', 'attributes'=>array('class'=>'placeholder')));
 
-	$menu->addChild('admins', array('label'=>'Admins','route' => 'admin_admins_list'));
-		$menu['admins']->addChild('list', array('label'=>'List Admins', 'route'=>'admin_admins_list', 'attributes'=>array('class'=>'placeholder')));
-		$menu['admins']->addChild('add', array('label'=>'Add New Admin', 'route'=>'admin_admins_add', 'attributes'=>array('class'=>'placeholder')));
+	$menu->addChild('admins', array('label'=>$t->trans('dashboard.admins'),'route' => 'admin_admins_list'));
+		$menu['admins']->addChild('list', array('label'=>$t->trans('dashboard.list_admins'), 'route'=>'admin_admins_list', 'attributes'=>array('class'=>'placeholder')));
+		$menu['admins']->addChild('add', array('label'=>$t->trans('dashboard.add_admin'), 'route'=>'admin_admins_add', 'attributes'=>array('class'=>'placeholder')));
 
         // ... add more children
 
