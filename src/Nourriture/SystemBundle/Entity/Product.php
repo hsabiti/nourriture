3f30ae7a7b5e245ea0e2ea3982753d0e468adce4
Henry Sabiti <hsabiti@hotmail.com>
2013-05-20 12:58:31 +0100

    product entity

---
+++ src/Nourriture/SystemBundle/Entity/Product.php
@@ -0,0 +1,18 @@
+<?php
+namespace Nourriture\SystemBundle\Entity;
+
+use Sylius\Bundle\AssortmentBundle\Entity\Product as BaseProduct;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Product extends BaseProduct
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
